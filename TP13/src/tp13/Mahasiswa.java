438bc4a36a17f79936f52990e463ccd15275434d
chiquitosaf <chiquitosaf@gmail.com>
2023-12-18 06:28:29 +0700

    first commit

---
+++ TP13/src/tp13/Mahasiswa.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp13;
+
+/**
+ *
+ * @author Chiquito
+ */
+public class Mahasiswa {
+    private String nama, nim, prodi;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    
+}
